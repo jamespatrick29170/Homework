e6a4009654a03961593195149b5dda586c9324b4
James Patrick <257362047+jamespatrick29170@users.noreply.github.com>
2026-09-03 14:31:45 -0400

    note adding functional now. added remove button to new notes.

--- js/script.js
+++ js/script.js
@@ -2,3 +2,26 @@ const noteInput = document.getElementById('noteInput')
 const addNote = document.getElementById('addNote')
 const notesList = document.getElementById('notesList')
 
+addNote.addEventListener('click', function() {
+    const noteText = noteInput.value;
+    
+    // create li element for new notes and store text
+    const newNote = document.createElement('li');
+    newNote.textContent = noteText;
+
+    // create remove button for note
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    
+    removeButton.addEventListener('click', function() {
+        notesList.removeChild(newNote);
+    })
+
+    //put button inside the note
+    newNote.appendChild(removeButton);
+
+    // add note into the li element itself
+    notesList.appendChild(newNote);
+    
+    noteInput.value = '';
+})
