0f468088993ceed0df4d51afaaf7beee4e46234e
James Patrick <257362047+jamespatrick29170@users.noreply.github.com>
2026-09-06 18:30:09 -0400

    added js for form and validation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
+//notelist variables
 const noteInput = document.getElementById('noteInput');
 const addNote = document.getElementById('addNote');
 const notesList = document.getElementById('notesList');
+//theme button varis
 const themeToggle = document.getElementById('themeToggle');
+//form varis
+const feedbackForm = document.getElementById('feedbackForm');
+const nameInput = document.getElementById('nameInput');
+const nameError = document.getElementById('nameError');
+const emailInput = document.getElementById('emailInput');
+const emailError = document.getElementById('emailError');
+const ratingInput = document.getElementById('ratingInput');
+const ratingError = document.getElementById('ratingError');
+const commentsInput = document.getElementById('commentsInput');
+const commentsError = document.getElementById('commentsError');
+
 
 addNote.addEventListener('click', function() {
     const noteText = noteInput.value;
@@ -41,4 +54,49 @@ noteInput.addEventListener('input', function() {
 
 themeToggle.addEventListener('click', function() {
     document.body.classList.toggle('alt-theme');
+})
+
+feedbackForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let isValid = true;
+    //name check
+    if (nameInput.value.trim() === '') {
+        nameError.textContent = 'Name is required';
+        isValid = false;
+    } else {
+        nameError.textContent = '';
+    }
+    //email checks
+    if (emailInput.value.trim() === '') {
+        emailError.textContent = 'Email is required';
+        isValid = false;
+    } else if (!emailInput.value.includes ('@')) {
+        emailError.textContent = 'Email must contain an @';
+        isValid = false;
+    } else {
+        emailError.textContent = '';
+    }
+    //rating check
+    if (ratingInput.value.trim() === '') {
+        ratingError.textContent = 'Rating is required';
+        isValid = false;
+    } else {
+        ratingError.textContent = '';
+    }
+    //comments check
+    if (commentsInput.value.trim() === '') {
+        commentsError.textContent = 'Comment is required';
+        isValid = false;
+    } else {
+        commentsError.textContent = '';
+    }
+    //console log if good
+    if (isValid) {
+        console.log('Feedback submitted:', nameInput.value, emailInput.value, ratingInput.value, commentsInput.value);
+        feedbackForm.reset();
+    }
+})
+
+emailInput.addEventListener('input', function() {
+    emailError.textContent = '';
 })
